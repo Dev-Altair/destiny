869f7c55a2cede77f63ac73d34223b33bf728f3f
Altair <91957784+Dev-Altair@users.noreply.github.com>
2021-10-27 20:20:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://destiny.devpage.me" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>About Me &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
